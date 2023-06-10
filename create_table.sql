dc72d80cd23f3d0e8ca2944d72308f5d802b7b15
cynthiaphlndr <cynthia.philander21@gmail.com>
2023-06-10 18:18:37 +0700

    finishing post user form

--- create_table.sql
+++ create_table.sql
@@ -10,3 +10,55 @@ CREATE TABLE users (
     age INT,
     bmi INT
 );
+
+CREATE TABLE foods (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(500),
+    recipeCategory  VARCHAR(500),
+    calories FLOAT,
+    fatContent FLOAT,
+    saturatedFatContent FLOAT,
+    cholesterolContent FLOAT,
+    sodiumContent FLOAT,
+    carbohydrateContent FLOAT,
+    fiberContent FLOAT,
+    sugarContent FLOAT,
+    proteinContent FLOAT
+);
+
+CREATE TABLE ingredients (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(500)
+);
+
+CREATE TABLE recipe (
+    foodID INT,
+    ingredientsID INT,
+    PRIMARY KEY (foodID, ingredientsID),
+    FOREIGN KEY (foodID) REFERENCES foods(id),
+    FOREIGN KEY (ingredientsID) REFERENCES ingredients(id)
+);
+
+CREATE TABLE allergies (
+    userID VARCHAR(16),
+    ingredientsID INT,
+    PRIMARY KEY (userID, ingredientsID),
+    FOREIGN KEY (userID) REFERENCES users(id),
+    FOREIGN KEY (ingredientsID) REFERENCES ingredients(id)
+);
+
+CREATE TABLE preference (
+    userID VARCHAR(16),
+    foodID INT,
+    PRIMARY KEY (userID, foodID),
+    FOREIGN KEY (userID) REFERENCES users(id),
+    FOREIGN KEY (foodID) REFERENCES foods(id)
+);
+
+CREATE TABLE recommendation (
+    userID VARCHAR(16),
+    foodID INT,
+    PRIMARY KEY (userID, foodID),
+    FOREIGN KEY (userID) REFERENCES users(id),
+    FOREIGN KEY (foodID) REFERENCES foods(id)
+);
--- src/server.js
+++ src/server.js
@@ -75,6 +75,156 @@ function getProfile(id){
     })
 }
 
+function postForm(height, weight, weightGoal, gender, age, bmi, id){
+    return new Promise((resolve, reject) => {
+        connection.query(`UPDATE users SET height='${height}', weight='${weight}', weightGoal='${weightGoal}', gender='${gender}', age='${age}', bmi='${bmi}' WHERE id='${id}'`, (err, result, fields)=>{
+            if(err){               
+                return resolve(false);
+            }
+
+            return resolve(true);
+        });
+    })
+}
+
+function getAllergiesID(allergies){
+    return new Promise((resolve, reject) => {
+        let allergiesID = [];
+        for(let i=0; i<allergies.length; i++){
+            connection.query(`SELECT id FROM ingredients where name='${allergies[i]}'`, (err, result, fields)=>{
+                if(!err){               
+                    console.log(result[0].id);
+                    allergiesID.push(result[0].id);
+                    console.log(allergies.length + " bertambah");
+                }
+                if(i==allergies.length-1){
+                    return resolve(allergiesID);
+                }
+            });
+        }
+        
+    })
+}
+
+async function postAllergies(allergies, id){
+    const allergiesID = await getAllergiesID(allergies);
+    return new Promise((resolve, reject) => {
+        console.log("lengthnya " + allergiesID.length);
+        console.log("get id alergi")
+        const insertAllergyPromises = allergiesID.map((allergyID) => {
+            return new Promise((resolve, reject) => {
+                console.log("masuk insert");
+              connection.query(
+                `INSERT INTO allergies (userID, ingredientsID) VALUES ('${id}', '${allergyID}')`,
+                (err, result, fields) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    resolve(result);
+                  }
+                }
+              );
+            });
+          });
+
+          Promise.all(insertAllergyPromises)
+          .then(() => {
+            return resolve(true);
+          })
+          .catch(() => {
+            return resolve(false);
+          });
+      });
+}
+
+function getPreferencesName(preferences){
+    return new Promise((resolve, reject) => {
+        let preferencesName = [];
+        for(let i=0; i<preferences.length; i++){
+            connection.query(`SELECT id FROM foods where name='${preferences[i]}'`, (err, result, fields)=>{
+                if(result.length==0){               
+                    preferencesName.push(preferences[i]);
+                }
+                if(preferences.length-1==i){
+                    return resolve(preferencesName);
+                }
+            });
+        }
+    })
+}
+
+
+async function postPreferences(preferences){
+    const listUnstoredPreferences = await getPreferencesName(preferences);
+    console.log("preferensi yang belum ada "+listUnstoredPreferences.length);
+    return new Promise((resolve, reject) => {
+        const insertPreferencesNamePromises = listUnstoredPreferences.map((preferenceName) => {
+            return new Promise((resolve, reject) => {
+                console.log("masuk insert lagi");
+                connection.query(`INSERT INTO foods (name) VALUES ('${preferenceName}')`, (err, result, fields)=>{
+                    if(err){               
+                        reject(err);
+                    }
+                    else{
+                        resolve(result);
+                    }
+                });
+            });
+          });
+
+          Promise.all(insertPreferencesNamePromises)
+          .then(() => {
+            return resolve(true);
+          })
+          .catch(() => {
+            return resolve(false);
+          });
+    })
+}
+
+async function getPreferencesID(preferences){
+    return new Promise((resolve, reject) => {
+        let preferencesID = [];
+        for(let i=0; i<preferences.length; i++){
+            connection.query(`SELECT id FROM foods where name='${preferences[i]}'`, (err, result, fields)=>{
+                    console.log("id preferensi "+result[0]);
+                    preferencesID.push(result[0].id);
+                    if(preferences.length-1==i){
+                        return resolve(preferencesID);
+                    }
+            });
+        }
+    })
+}
+
+
+
+async function postPreferencesID(preferences, id){
+    const preferencesID = await getPreferencesID(preferences);
+    return new Promise((resolve, reject) => {
+        const insertPreferencesIDPromises = preferencesID.map((preferenceID) => {
+            return new Promise((resolve, reject) => {
+                console.log("masuk insert lagi");
+                connection.query(`INSERT INTO preference (userID, foodID) VALUES ('${id}', '${preferenceID}')`, (err, result, fields)=>{
+                    if(err){               
+                        console.log(err);
+                    }
+            
+                    resolve(result);
+                });
+            });
+          });
+
+          Promise.all(insertPreferencesIDPromises)
+          .then(() => {
+            return resolve(true);
+          })
+          .catch(() => {
+            return resolve(false);
+          });
+    })
+}
+
   server.route({
     method: 'POST',
     path: '/register',
@@ -154,9 +304,6 @@ function getProfile(id){
   server.route({
     method: 'GET',
     path: '/user/{id}',
-    options: {
-        auth: 'jwt' // Require authentication using the 'jwt' strategy
-      },
     handler: async function(request, h) {
 
         const {
@@ -173,9 +320,21 @@ function getProfile(id){
             response = h.response({
             status: 'success',
             message: 'success',
-            data: {
-                user: error
-            },
+            data1: 
+                [
+                    {
+                        id: error[0].id,
+                        name: error[0].name,
+                        email: error[0].email,
+                        password: error[0].password,
+                        height: error[0].height,
+                        weight: error[0].weight,
+                        weightGoal: error[0].weightGoal,
+                        gender: error[0].gender,
+                        age: error[0].age,
+                        bmi: error[0].bmi
+                    }
+                ],
             });
             response.code(201);
             return response;
@@ -189,6 +348,55 @@ function getProfile(id){
         return response;
     
   }});
+
+  server.route({
+    method: 'POST',
+    path: '/user/form/{id}',
+    handler: async function(request, h) {
+
+        const {
+            id
+        } = request.params;
+        
+        console.log("req param");
+
+        const {
+            height, weight, weightGoal, gender, age, bmi, allergies, preferences
+        } = request.payload;
+
+        console.log("req payload");
+
+        const form = await postForm(height, weight, weightGoal, gender, age, bmi, id);
+
+        console.log("postForm");
+
+        const allergiesForm = await postAllergies(allergies, id);
+        console.log("post allergies");
+
+        const postUnstoredPreferences = await postPreferences(preferences);
+        console.log("post unstored preferences");
+
+        const postPreferencesIDD = await postPreferencesID(preferences, id);
+        console.log("post id preferensi");
+
+        if(postPreferencesID){
+            response = h.response({
+            status: 'success',
+            message: 'success',
+            });
+            response.code(201);
+            return response;
+        }
+
+        response = h.response({
+        status: 'fail',
+        message: 'error',
+        });
+        response.code(400);
+        return response;
+    
+  }});
+
 
   await server.start();
 };
